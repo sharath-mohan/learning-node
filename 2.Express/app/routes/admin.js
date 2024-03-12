5ec8b456dc4fea1ef8b5bd7456f3cf91f592adb8
sharath-mohan <sharathdesignsofficial@gmail.com>
2024-03-12 23:47:56 +0530

    chapter 2: express

---
+++ 2.Express/app/routes/admin.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+router.get("/add-product", (req, res) => {
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+});
+module.exports = router;
